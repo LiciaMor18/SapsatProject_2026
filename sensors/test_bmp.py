d064b83881e49f8683bc320325f79104a4b70754
mor4z <morazio.2018794@studenti.uniroma1.it>
2025-12-20 12:28:07 +0100

    bmp_test and test_buzzer

--- sensors/test_bmp.py
+++ sensors/test_bmp.py
@@ -3,6 +3,10 @@ import bmp280
 import time
 import math
 
+
+def altitude(T_0, p_t, p_0):
+    return T_0 / LAMBDA * (1 - (p_t / p_0)**EXP)
+    
 # Initialize I2C communication
 i2c = I2C(1, sda=Pin(10), scl=Pin(11), freq=100000) # I2C bus 1, SDA pin GP10, SCL pin GP11,
 
@@ -22,7 +26,6 @@ M = 0.0290
 EXP = - (LAMBDA * R) / (g * M)
 
 
-
 h_0 = T_0 / LAMBDA * (1 - 1**EXP)
 print(f'p_0 = {p_0}, T_0 = {T_0}, h_0 = {h_0}, LAMBDA = {LAMBDA}, EXP = {EXP}, ')
 
@@ -32,7 +35,7 @@ while True:
     
     T_t = bmp.temperature
     p_t = bmp.pressure
-    h_t = T_0 / LAMBDA * (1 - (p_t / p_0)**EXP)
+    h_t = altitude(T_0, p_t, p_0)
 
     # Print temperature and pressure data
     print(f'p_t = {p_t}, T_t = {T_t}, h_t = {h_t}')
